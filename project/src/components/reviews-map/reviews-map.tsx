6626f1796c41e0605e568f22a68e4df2ddc98db8
Egor Dukov <dkffe2@gmail.com>
2022-03-14 10:00:59 +0300

    1. Прошлые замечания

--- project/src/components/reviews-map/reviews-map.tsx
+++ project/src/components/reviews-map/reviews-map.tsx
@@ -1,8 +1,10 @@
 import { Offers } from '../../types/offer';
 import Map from '../map/map';
+
 type reviewsMapProps = {
   offers: Offers,
 }
+
 export default function ReviewsMap({ offers }: reviewsMapProps) {
   return (
     <Map className={'property__map'} offers={offers} />
